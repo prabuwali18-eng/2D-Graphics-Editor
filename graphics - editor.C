3af886f7a3e54e334c3d6c61f23fd6a4fa302f6e
Praveen B Wali <prabuwali18@gmail.com>
2026-05-30 20:07:05 +0530

    Added C graphics editor

---
+++ graphics - editor.C
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+#define ROWS 10
+#define COLS 20
+
+char canvas[ROWS][COLS];
+
+void initializeCanvas()
+{
+    for(int i=0;i<ROWS;i++)
+       for(int j=0;j<COLS;j++)
+          canvas[i][j]='_';
+
+}
+void displayCanvas()
+{
+    for(int i=0;i<ROWS;i++)
+    {
+        for(int j=0;j<COLS;j++)
+        printf("%c",canvas[i][j]);
+        printf("\n");
+    }
+}
+void drawPoint(int row,int col)
+{
+    if(row >=0 && row < ROWS && col >= 0 && col < COLS)
+      canvas[row][col]='*';
+}
+int main()
+{
+    initializeCanvas();
+    drawPoint(2,5);
+    displayCanvas();
+
+    return 0;
+}
