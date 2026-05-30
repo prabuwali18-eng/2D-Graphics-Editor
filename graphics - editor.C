06bfd24cf04ad5b2fd66bcb8779d38e666307ab1
Praveen B Wali <prabuwali18@gmail.com>
2026-05-30 22:17:04 +0530

    Added Rectangle funcationally

--- graphics - editor.C
+++ graphics - editor.C
@@ -34,11 +34,29 @@ void drawHorizontalLine(int row)
         canvas[row][col] = '*';
     }
 }
+void drawRectangle(int row,int col,int height,int width)
+{
+    for(int i=row;i<row + height;i++)
+    {
+        for(int j=col;j<col + width;j++)
+        {
+            if(i >= 0 && i < ROWS && j >= 0 && j < COLS)
+            {
+                if(i == row || i == row + height - 1 ||
+                   j == col || j == col + width - 1)
+                   {
+                    canvas[i][j] = '*';
+                   }
+            }
+        }
+    }
+}
 int main()
 {
     initializeCanvas();
     drawPoint(2,5);
     drawHorizontalLine(4);
+    drawRectangle(2,3,5,10);
     displayCanvas();
 
     return 0;
