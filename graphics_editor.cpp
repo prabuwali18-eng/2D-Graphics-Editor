5deeab810b9e9f1907916b259ce5d33910c29692
Praveen B Wali <prabuwali18@gmail.com>
2026-05-30 15:20:49 +0530

    Initial 2D graphics editor setup

--- graphics_editor.cpp
+++ graphics_editor.cpp
@@ -28,12 +28,20 @@ void drawPoint(int row,int col)
     if(row >=0 && row < ROWS && col >= 0 && col < COLS)
       canvas[row][col]='*';
 }
+void drawHorizontalLine(int row)
+{
+    for(int col=0;col < COLS;col++)
+    {
+        canvas[row][col] = '*';
+    }
+}
 
 int main()
 {
     initializeCanvas();
 
     drawPoint(2,5);
+    drawHorizontalLine(4);
 
     displayCanvas();
 
